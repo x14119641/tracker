da24be86441e23c1b6eeeebb6feae36bba5b6ae4
x14119641 <daniel.gil.romero24@gmail.com>
2022-02-02 23:02:07 +0100

    Till db

---
+++ app/routes.py
@@ -0,0 +1,22 @@
+from app import app
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
+
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
